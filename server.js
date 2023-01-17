6f2640af0835595e3519204117a3692f4928cdc8
annesh08 <anneshdodo@gmail.com>
2023-01-17 17:48:16 +0530

    Added Express router and separated the routes file

--- server.js
+++ server.js
@@ -1,42 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 
+//Import Route Files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.get('/api/v1/bootcamps', (req,res)=> {
-    res.status(200).json({
-        success: true,
-        message: 'Show all bootcamps'
-    });
-});
-
-app.get('/api/v1/bootcamps/:id', (req,res)=> {
-    res.status(200).json({
-        success: true,
-        message: `Display bootcamp ${req.params.id}`
-    });
-});
-
-app.post('/api/v1/bootcamps', (req,res)=> {
-    res.status(200).json({
-        success: true,
-        message: 'Create new bootcamp'
-    });
-});
-
-app.put('/api/v1/bootcamps/:id', (req,res)=> {
-    res.status(200).json({
-        success: true,
-        message: `Update bootcamp ${req.params.id}`
-    });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req,res)=> {
-    res.status(200).json({
-        success: true,
-        message: `Delete bootcamp ${req.params.id}`
-    });
-});
+//Mount Router
+app.use('/api/v1/bootcamps', bootcamps);
 
 const port = process.env.PORT || 3000;
 
