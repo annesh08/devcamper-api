effe422af414cf9f0c78d207e4e8321ee0473180
annesh08 <anneshdodo@gmail.com>
2023-01-18 14:02:14 +0530

    Added morgan for dev env

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
 
 //Import Route Files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
+//Add Logger middleware for development
+if ((process.env.NODE_ENV = "development")) {
+  app.use(morgan("common"));
+}
+
 //Mount Router
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Listening at http://localhost:${port}/`);
-})
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
