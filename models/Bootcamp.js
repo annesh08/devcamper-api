b2ff41107296fe8a166d416da16f875a127aff2a
annesh08 <anneshdodo@gmail.com>
2023-01-19 22:30:00 +0530

    Started creating models

---
+++ models/Bootcamp.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const bootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name cannot be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please enter a description"],
+    maxlength: [500, "Description cannot be more than 500 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can not be longer than 20 characters"],
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    //GeoJSON Point
+    type: {
+      type: String,
+      enums: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+  },
+});
