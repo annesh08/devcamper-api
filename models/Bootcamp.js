439cd5bc6164d96d671f9c1f2b7fcaa422a1f0d3
annesh08 <anneshdodo@gmail.com>
2023-01-20 14:58:32 +0530

    Created bootcamp schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -49,5 +49,55 @@ const bootcampSchema = new mongoose.Schema({
       index: "2dsphere",
     },
     formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    //Array of Strings
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other",
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be atleast 1"],
+    max: [10, "Rating cannot be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
+
+module.exports = mongoose.model("Bootcamp", bootcampSchema);
